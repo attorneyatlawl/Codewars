30b41e775a0fc3cf16fdb62c94502931f56aaf8a
Thomas W. Langevin <dubba_5@hotmail.com>
2016-12-10 16:32:50 -0500

    Add files via upload

---
+++ Popultation Growth.py
@@ -0,0 +1,12 @@
+''' Population Growth
+
+In a small town the population is p0 = 1000 at the beginning of a year. 
+The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. 
+How many years does the town need to see its population greater or equal to p = 1200 inhabitants?'''
+
+def nb_year(p0, percent, aug, p):
+    count = 0
+    while p0 < p:
+        count += 1
+        p0 = p0+ (p0 * (percent/100)) + aug # had to remember to make the percent number a percentage
+    return count
